fdcb4a329253b9796e8ba917025f16fa9313bb6a
emreozum <h.emreozum@gmail.com>
2022-12-04 17:24:40 +0300

    Update index.js
    Dealer added

--- index.js
+++ index.js
@@ -1,20 +1,30 @@
 let player = {
-    name: "Per",
+    name: "Emre",
     chips: 200,
     }
 
+let dealer = {
+    name: "Dealer",
+    chips: 200,
+    }
 
-let cards = []
-let sum = 0
+let playerCards = []
+let dealerCards = []
+let playerSum = 0
+let dealerSum = 0
 let hasBlackJack = false
 let isAlive = false
 let message = ""
 let messageEl = document.getElementById("message-el")
-let sumEl = document.getElementById("sum-el")
-let cardsEl = document.getElementById("cards-el")
+let playerSumEl = document.getElementById("player-sum-el")
+let dealerSumEl = document.getElementById("dealer-sum-el")
+let playerCardsEl = document.getElementById("player-cards-el")
+let dealerCardsEl = document.getElementById("dealer-cards-el")
 let playerEl = document.getElementById("player-el")
+let dealerEl = document.getElementById("dealer-el")
 
 playerEl.textContent = player.name + ": $" + player.chips
+dealerEl.textContent = dealer.name + ": $" + dealer.chips
 
 function getRandomCard() {
     let randomNumber = Math.floor( Math.random()*13 ) + 1
@@ -30,41 +40,66 @@ function getRandomCard() {
 function startGame() {
     hasBlackJack = false
     isAlive = true
-    let firstCard = getRandomCard()
-    let secondCard = getRandomCard()
-    cards = [firstCard, secondCard]
-    sum = firstCard + secondCard
+    let playerfirstCard = getRandomCard()
+    let playersecondCard = getRandomCard()
+    playerCards = [playerfirstCard, playersecondCard]
+    playerSum = playerfirstCard + playersecondCard
+
+    let dealerfirstCard = getRandomCard()
+    let dealersecondCard = getRandomCard()
+    dealerCards = [dealerfirstCard, dealersecondCard]
+    dealerSum = dealerfirstCard + dealersecondCard
+
     renderGame()
 }
 
 function renderGame() {
-    cardsEl.textContent = "Cards: "
-    for (let i = 0; i < cards.length; i++) {
-        cardsEl.textContent += cards[i] + " "
+    playerCardsEl.textContent = "Player Cards: "
+    dealerCardsEl.textContent = "Dealer Cards: "
+    for (let i = 0; i < playerCards.length; i++) {
+        playerCardsEl.textContent += playerCards[i] + " "
+    }
+    for (let i = 0; i < dealerCards.length; i++) {
+        dealerCardsEl.textContent += dealerCards[i] + " "
     }
     
-    sumEl.textContent = "Sum: " + sum
-    if (sum <= 20) {
+    playerSumEl.textContent = "Player Sum: " + playerSum
+    dealerSumEl.textContent = "Dealer Sum: " + dealerSum
+    if ((playerSum || dealerSum) <= 20) {
         message = "Do you want to draw a new card?"
-    } else if (sum === 21) {
+    } else if (playerSum === 21) {
         message = "You've got Blackjack!"
         hasBlackJack = true
         playerEl.textContent = player.name + ": $" + (player.chips += 10)
-    } else {
-        message = "You're out of the game!"
+        dealerEl.textContent = dealer.name + ": $" + (dealer.chips -= 10)
+    } else if (dealerSum === 21) {
+        message = "Dealer has Blackjack!"
+        hasBlackJack = true
+        playerEl.textContent = player.name + ": $" + (player.chips -= 10)
+        dealerEl.textContent = dealer.name + ": $" + (dealer.chips += 10)
+    } else if (playerSum > 21) {
+        message = "You Lost!"
         isAlive = false
         playerEl.textContent = player.name + ": $" + (player.chips -= 10)
+        dealerEl.textContent = dealer.name + ": $" + (dealer.chips += 10)
         
+    } else if (dealerSum > 21) {
+        message = "You Won!"
+        isAlive = false
+        playerEl.textContent = player.name + ": $" + (player.chips += 10)
+        dealerEl.textContent = dealer.name + ": $" + (dealer.chips -= 10)
+        messageEl.textContent = message
     }
-    messageEl.textContent = message
 }
 
 
 function newCard() {
     if (isAlive === true && hasBlackJack === false) {
         let card = getRandomCard()
-        sum += card
-        cards.push(card)
+        playerSum += card
+        playerCards.push(card)
+        dealerSum += card
+        dealerCards.push(card)
         renderGame()        
     }
 }
